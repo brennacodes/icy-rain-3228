297bf2fe80914287e8b9e066647b5a56467ddb52
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-08-09 09:58:53 -0600

    Update passenger model; add associations; scope :minor

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,7 @@
 class Passenger < ApplicationRecord
+  has_many :flights_passengers
+  has_many :flights, through: :flights_passengers
+  has_many :airlines, through: :flights
+
+  scope :minor, -> { where('age <= ?', 18) }
 end
