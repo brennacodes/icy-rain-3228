0f40cab65087fc9b2942c745eca7d090074579cf
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-08-09 10:06:00 -0600

    Update airline and passenger models

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -4,6 +4,10 @@ class Airline < ApplicationRecord
   has_many :flights_passengers, through: :passengers
 
   def adult_passengers
-    passengers.where(age: '>= 18')
+    passengers.adults.uniq
+  end
+
+  def sort_passengers_by_number_of_flights
+    passengers.adults.group('passengers.id').order('count(flights_passengers.passenger_id) DESC')
   end
 end
