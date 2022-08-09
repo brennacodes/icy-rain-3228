20b812885b6c676bf86687d53ca2696f07142362
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-08-09 10:35:37 -0600

    Update airline show spec

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -9,11 +9,18 @@ RSpec.describe 'airlines show page', type: :feature do
   let!(:passenger_1) { Passenger.create(name: 'John Doe', age: 33) }
   let!(:passenger_2) { Passenger.create(name: 'Gary Garrison', age: 22) }
   let!(:passenger_3) { Passenger.create(name: 'Willy Wonka', age: 77) }
-  let!(:passenger_4) { Passenger.create(name: 'John Doe', age: 44) }
+  let!(:passenger_4) { Passenger.create(name: 'Kim Kusack', age: 44) }
+  let!(:passenger_5) { Passenger.create(name: 'Molly Morrison', age: 55) }
   let!(:flights_passenger_1) { FlightsPassenger.create(flight_id: flight_1.id, passenger_id: passenger_1.id) }
   let!(:flights_passenger_2) { FlightsPassenger.create(flight_id: flight_1.id, passenger_id: passenger_2.id) }
   let!(:flights_passenger_3) { FlightsPassenger.create(flight_id: flight_2.id, passenger_id: passenger_3.id) }
   let!(:flights_passenger_4) { FlightsPassenger.create(flight_id: flight_2.id, passenger_id: passenger_1.id) }
+  let!(:flights_passenger_5) { FlightsPassenger.create(flight_id: flight_3.id, passenger_id: passenger_4.id) }
+  let!(:flights_passenger_6) { FlightsPassenger.create(flight_id: flight_3.id, passenger_id: passenger_5.id) }
+  let!(:flights_passenger_7) { FlightsPassenger.create(flight_id: flight_4.id, passenger_id: passenger_4.id) }
+  let!(:flights_passenger_8) { FlightsPassenger.create(flight_id: flight_4.id, passenger_id: passenger_3.id) }
+  let!(:flights_passenger_9) { FlightsPassenger.create(flight_id: flight_4.id, passenger_id: passenger_2.id) }
+  let!(:flights_passenger_10) { FlightsPassenger.create(flight_id: flight_4.id, passenger_id: passenger_1.id) }
 
   before do
     visit airline_path(airline_1)
@@ -55,14 +62,6 @@ RSpec.describe 'airlines show page', type: :feature do
   end
 
   it 'sorts list of passengers by number of flights each has taken on that airline' do
-    passenger_4 = Passenger.create(name: 'Jane Doe', age: 40)
-    flights_passenger_5 = FlightsPassenger.create(flight_id: flight_1.id, passenger_id: passenger_4.id)
-    flights_passenger_6 = FlightsPassenger.create(flight_id: flight_2.id, passenger_id: passenger_2.id)
-    flights_passenger_7 = FlightsPassenger.create(flight_id: flight_3.id, passenger_id: passenger_1.id)
-    flights_passenger_8 = FlightsPassenger.create(flight_id: flight_3.id, passenger_id: passenger_2.id)
-    flights_passenger_9 = FlightsPassenger.create(flight_id: flight_3.id, passenger_id: passenger_3.id)
-    flights_passenger_10 = FlightsPassenger.create(flight_id: flight_4.id, passenger_id: passenger_1.id)
-
     within "#passenger_#{passenger_1.id}" do
       expect(page).to have_content("#{passenger_1.name}")
     end
@@ -79,8 +78,11 @@ RSpec.describe 'airlines show page', type: :feature do
       expect(page).to have_content("#{passenger_4.name}")
     end
 
-      expect(passenger_1.name).to appear_before("#{passenger_2.name}")
-      expect(passenger_2.name).to appear_before("#{passenger_3.name}")
-      expect(passenger_3.name).to appear_before("#{passenger_4.name}")
+    sorted_passengers = airline_1.sort_passengers_by_number_of_flights
+
+    expect(sorted_passengers[0].name).to eq(passenger_1.name)
+    expect(sorted_passengers[1].name).to eq(passenger_2.name)
+    expect(sorted_passengers[2].name).to eq(passenger_3.name)
+    expect(sorted_passengers[3].name).to eq(passenger_4.name)
   end
 end
